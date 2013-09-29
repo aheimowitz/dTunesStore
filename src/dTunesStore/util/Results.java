e96e2c32030c4656b62271af3a6e6dc2470461fc
Mike <mike@DeathStar.(none)>
2013-09-29 16:32:05 -0400

    Added to the Results class

--- src/dTunesStore/util/Results.java
+++ src/dTunesStore/util/Results.java
@@ -1,7 +1,26 @@
-
 package dTunesStore.util;
+//---------------------------------------------------------------------
+import dTunesStore.dataStore.MusicInfo;
+import java.util.Hashtable;
+import java.util.Enumeration;
+//---------------------------------------------------------------------
+public class Results 
+{
+	Hashtable <String, MusicInfo> songList;
+	/**
+	*	This method is used to display all of the song entries 
+	*	 that are in the library.
+	**/
+	public void displayData()
+	{
+		//Makes an enum based on all the keys in the songList
+		Enumeration keys = songList.keys();
 
-public class Results {
-
+		//Iterates through the keys and prints all entries out
+		while(keys.hasMoreElements())
+		{
+			System.out.println(songList.get(keys.nextElement()));
+		}
+	}
 
 }
